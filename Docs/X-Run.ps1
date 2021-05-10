1740abccbeb333182c3a2065d50f9babc756fa17
Joma <megamanx@outlook.com>
2021-05-10 06:09:26 -0500

    Fixed. Bug. Different fingerprint in same browser instance.

--- Docs/X-Run.ps1
+++ Docs/X-Run.ps1
@@ -9,7 +9,7 @@ Write-Host
 $browser = "chrome" #"$(Select-ValueByPlatform "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" "TBD" "TBD")"
 $browserArgs = "-new-window -guest"
 
-Write-InfoYellow "Close the browser first and press Ctrl+C to exit!"
+Write-InfoYellow "Press Ctrl+C to exit!"
 
 & $env:EMSCRIPTEN_EMRUN "index.html" --browser $browser --browser_args $browserArgs --browser_info
 Write-InfoDarkGreen "█ End run Insane.js - Test client"
