8c896b707160c9434a9532017bb7f04fb609efa9
Joma <megamanx@outlook.com>
2021-05-10 05:28:57 -0500

    fixed TestDependencyTools script

--- X-TestDependencyTools.ps1
+++ X-TestDependencyTools.ps1
@@ -6,9 +6,17 @@ Write-Host
 Write-InfoDarkGray "███ Test - Dependency Tools"
 Write-Host
 
+Write-InfoMagenta "git --version"
 git --version; Test-LastExitCode
+Write-Host
+
+Write-InfoMagenta "make --version"
 make --version; Test-LastExitCode
+Write-Host
+
+Write-InfoMagenta "java --version"
 java --version; Test-LastExitCode
+Write-Host
 
 Write-InfoDarkGray "█ End - Test - Dependency Tools"
 Write-Host
