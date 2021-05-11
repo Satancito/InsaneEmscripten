2b8477e5b05fa190f9091a90a58d9f753a3430f1
Joma <megamanx@outlook.com>
2021-05-11 11:52:47 -0500

    X-Run script fixed. Added msedge support for Windows and MacOS.

--- Docs/X-Run.ps1
+++ Docs/X-Run.ps1
@@ -1,12 +1,28 @@
 $ErrorActionPreference = "Stop"
 Import-Module -Name "$(Get-Item "./Z-CoreFxs*.ps1")" -Force -NoClobber
-Write-InfoDarkGray "▶▶▶ Running: $PSCommandPath"
+Write-InfoDarkGray "▶️▶️▶️ Running: $PSCommandPath"
 
 Write-Host
 Write-InfoDarkGreen "████ Run Insane.js - Test client"
 Write-Host
 & "./X-SetEnvVars.ps1"
-$browser = "chrome" #"$(Select-ValueByPlatform "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" "TBD" "TBD")"
+$browser = "chrome"
+$edge = "$(Select-ValueByPlatform "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" "TBD" "/Applications/Microsoft Edge.app/Contents/MacOS/Microsoft Edge")"
+if($IsWindows)
+{
+    if(Test-Path "$edge" -PathType Leaf)
+    {
+        $browser = "$edge"
+    }
+}
+if($IsMacOS)
+{
+    Write-Host $edge
+    if(Test-Path "$edge" -PathType Leaf)
+    {
+        $browser = "$edge"
+    }
+}
 $browserArgs = "-new-window -guest"
 
 Write-InfoYellow "Press Ctrl+C to exit!"
